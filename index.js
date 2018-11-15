c6ae3e84fa7d57ec07e8a453ba3d6aaf7fedd7bb
“Dg” <jeong214132@naver.com>
2018-11-16 00:14:32 +0900

    add timer, add start restart button, add gameover screen

--- index.js
+++ index.js
@@ -4,19 +4,69 @@ var generateRandom = function (min, max) {
     var ranNum = Math.floor(Math.random()*(max-min+1)) + min;
     return ranNum;
   }
-var result = generateRandom(0, 9);
+var result = generateRandom(1, 9);
 document.getElementById('div'+result).classList.add('black');
-let i=0;
-tap();
+let i;
+let time;
+
+document.getElementById('start').onclick=function(){
+    start();
+}
+document.getElementById('restart').onclick=function(){
+    document.getElementById('over').classList.add('hidden');
+    document.getElementById('start').classList.remove('hidden');
+    document.getElementById('restart').classList.add('hidden');
+    start();
+}
+
+
+function start(){
+    
+    i=0;
+    time=10;
+    document.getElementById("result").innerHTML=i;
+    document.getElementById("time").innerHTML=time;
+    setTimeout(timer, 1000);
+}
+function timer(){
+    time--;
+    error();
+    tap();
+    if(time<0){
+        document.getElementById('over').classList.remove('hidden');
+        document.getElementById('start').classList.add('hidden');
+        document.getElementById('restart').classList.remove('hidden');
+    }
+    else{
+        document.getElementById("time").innerHTML=time;
+        setTimeout(timer, 1000);
+    }
+    
+}
+function error(){
+    var whites=document.getElementsByClassName('white');
+    var i;
+    for(i=0;i<whites.length;i++){
+        whites.item(i).onclick=function(){
+            time=0;
+            document.getElementById("result").innerHTML=i;
+            document.getElementById("time").innerHTML=time;
+            document.getElementById('over').classList.remove('hidden');
+            document.getElementById('start').classList.add('hidden');
+            document.getElementById('restart').classList.remove('hidden');
+        }
+    }
+}
 function tap(){
     document.getElementsByClassName('black').item(0).onclick=function(){
         console.log(this);
         this.classList.remove('black');
-        var result = generateRandom(0, 9);
+        var result = generateRandom(1, 9);
         document.getElementById('div'+result).classList.add('black');
         i++;
         delete this;
-        tap();
         document.getElementById("result").innerHTML=i;
+        error();
+        tap();
     }
-  }
+}
